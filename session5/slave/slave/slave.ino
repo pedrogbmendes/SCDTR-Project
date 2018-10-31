d400c308f4870e899a0d1541c7fa8cf789a399da
pedro.goncalo.123@gmail.com <pedro.goncalo.123@gmail.com>
2018-10-31 15:17:29 +0000

    merd

---
+++ session5/slave/slave/slave.ino
@@ -0,0 +1,20 @@
+#include <Wire.h>
+
+const int own_address = 4; //this dev address
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin(own_address); // receive data
+  Wire.onReceive(receiveEvent); //event handler
+}
+
+void loop() {
+// EMPTY: all the work is done in receiveEvent
+  }
+  
+void receiveEvent(int howMany) {
+  while(Wire.available() > 0) { //check data on BUS
+  char c = Wire.read(); //receive byte at I2S BUS
+  Serial.write(c); // echo on terminal
+  }
+}
